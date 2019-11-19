d538ccf6e77d1dd5e3efecbbc058c739f5afa2b4
Henry Phan <h.phan06@gmail.com>
2019-11-19 03:40:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,3 +19,32 @@ class String
   end
   
 end
+
+  # def sentence?
+  #   if self.end_with?(".")
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  # def question?
+  #   if self.end_with?("?")
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  # def exclamation?
+  #   if self.end_with?("!")
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  # def count_sentences
+  #   new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+  #   return new_array.length
+  # end
