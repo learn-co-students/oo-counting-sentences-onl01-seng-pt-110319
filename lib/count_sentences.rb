fce3358707d364d2a03be75af498e48125f9f3f3
Wills Blake <richwblake@gmail.com>
2019-11-18 21:53:03 -0500

    Final commit, all tests pass

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    sentence_total = 0
+    count = 0
+    while count < self.length do
+      sentence_total += 1 if [".", "?", "!"].include?( self[count] ) && self[count] != self[count + 1]
+      count += 1
+    end
+    sentence_total
   end
 end
