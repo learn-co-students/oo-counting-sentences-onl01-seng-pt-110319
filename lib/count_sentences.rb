3377906dd01d404a0a6d2f5c5dc8041f220b75ae
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-21 11:44:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    if self.end_with?("."||"!"||"?")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+  return true   if self.end_with?("?")
+  return false  if !self.end_with?("?")
   end
 
   def exclamation?
-
+  return  true if self.end_with?("!")
+  return  false if !self.end_with?("!")
   end
 
   def count_sentences
-
+  p  @punctuation =self.split(/[.!?][^.{3}]/)
+  p  @punctuation.count
   end
 end
