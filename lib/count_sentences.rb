c88ebf782a81af75d9638e6caadccded602f8cea
qiong sun <joannesun44@gmail.com>
2019-12-14 21:31:19 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
 require 'pry'
 
 class String
+  
+  attr_accessor :sentence, :question, :exclamation, :count_sentences
+  
+  def initialize (sentence, question, exclamation, count_sentences)
+   @sentence = sentence
+   @question = question
+   @exclamation = exclamation
+   @count_sentences = count_sentences
+  end 
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+  self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
